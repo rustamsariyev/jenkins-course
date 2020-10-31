cca4b23d77ed36096708d05406cac2b1f9dee331
root <root@ip-172-31-17-135.ec2.internal>
2020-10-31 14:54:25 +0000

    Jobs DSL updated

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,7 +1,7 @@
 job('NodeJS example') {
     scm {
         git('git://github.com/rustamsariyev/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('Rustam Sariyeb')
+            node / gitConfigName('Rustam Sariyev')
             node / gitConfigEmail('r.sariyev@outlook.com')
         }
     }
